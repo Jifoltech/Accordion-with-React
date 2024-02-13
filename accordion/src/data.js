b169aa2f2a35043fd07bd1af8bf776da770de3c9
Olabode Ifeoluwa Jeremiah <91966577+Jifoltech@users.noreply.github.com>
2024-02-13 15:53:42 +0100

    Add files via upload

---
+++ accordion/src/data.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    id: 1,
+    question: "lorem ipsum dolor sit amet",
+    answer:
+      "Lorem ipsum, consectetur, sed do eiusmod tempor inc tempor incididunt ut lab et dolore magna aliqua. Ut enim ad minim ven",
+  },
+  {
+    id: 2,
+    question: "Lorem Ipsum dolor sit amet, consectetur adipiscing",
+    answer:
+      "Lorem ipsum, consectetur adipis lobortis non proident et non proident et, consectetur adipis lobort is proident et",
+  },
+  {
+    id: 3,
+    question: "Lorem Ipsum dolor sit amet, consectetur adipis",
+    answer:
+      "Ipsum, consectetur adipis, sed do eiusmod tempor incididunt ut labore et dol suites et d, ull clause nulla pariatur et d, ull s customize",
+  },
+  {
+    id: 4,
+    question:
+      "Lorem Ipsum dolor sit amet, consectetur adipis, consectetur adipiscing",
+    answer:
+      "Lorem, consectetur adipis, sed do eiusmod tempor inc idididunt ut labore et dol suites et d, ull, cliquis nisl aliqu Lorem Ipsum dolor sit amet et",
+  },
+];
+
+export default data;
